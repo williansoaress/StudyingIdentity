076658aaad685fd22cd368ffd4db97b62098e604
williansoaress <dasilva.williansoars@gmail.com>
2021-08-05 16:01:52 -0300

    Adding the identity tables

--- src/StudyingIdentity.App/Startup.cs
+++ src/StudyingIdentity.App/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StudyingIdentity.App.Data;
 
 namespace StudyingIdentity.App
 {
@@ -17,6 +19,7 @@ namespace StudyingIdentity.App
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
