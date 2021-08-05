6a87b273bf9838fae850ec3b11c160007fb1d60d
williansoaress <dasilva.williansoars@gmail.com>
2021-08-05 18:13:12 -0300

    Add login logic

--- src/StudyingIdentity.App/Controllers/AccountController.cs
+++ src/StudyingIdentity.App/Controllers/AccountController.cs
@@ -55,6 +55,31 @@ namespace StudyingIdentity.App.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+        }
+
         private void AddErrors(IdentityResult result)
         {
             foreach (var error in result.Errors)
